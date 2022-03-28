b728fad1d09da3d4346292e1f4b7fea6696c9113
frederikkjaersorensen <au645855@uni.au.dk>
2022-03-28 20:45:58 +0200

    f

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        HEJ
+        HEJ MED DIG
       </header>
     </div>
   );
