b86c69ed1ebd450190aca1212f1015ec7d2ef1ff
frederikkjaersorensen <au645855@uni.au.dk>
2022-03-30 21:00:14 +0200

    Tilføjede navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Siden <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/joke" className="nav-links" onClick={closeMobileMenu}>
+                Joke
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/food" className="nav-links" onClick={closeMobileMenu}>
+                Food
+              </Link>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
