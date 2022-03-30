5e8dcb2d36aba2fa572bd1bd03e05500509f77d9
frederikkjaersorensen <au645855@uni.au.dk>
2022-03-30 21:06:22 +0200

    Jokes

--- src/components/Pages/Joke.js
+++ src/components/Pages/Joke.js
@@ -1,32 +1,33 @@
-import '../../App.css'
+import "../../App.css";
 import React, { useState } from "react";
-
-
-
-
- 
+import "./Joke.css";
 
 function Joke() {
   const [insult, setInsult] = useState("");
 
-const axios = require("axios");
+  const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://insult.mattbas.org/api/insult',
+  function newInsult(){
+    const options = {
+      method: "GET",
+      url: "https://insult.mattbas.org/api/insult",
+    };
+  
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setInsult(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-  setInsult(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
   return (
     <>
-    <h1>Hello, {insult}!</h1>
-     
+      <h1>Hello, {insult}!</h1>
+      <button className="btn" onClick={newInsult}>INSULT</button>
     </>
   );
 }
