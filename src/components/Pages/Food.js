a339f4ea31fd07afd190b42522ecfe04d31714fe
Frederik Kjær Sørensen <202004444@post.au.dk>
2022-05-09 19:37:53 +0200

    Virker overhovedet ikke

--- src/components/Pages/Food.js
+++ src/components/Pages/Food.js
@@ -1,22 +1,42 @@
 import "../../App.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import DbService from "../../services";
 
 function Food() {
   const [newFood, setNewFood] = useState("");
   const [mealOfTheDay, setMealOfTheDay] = useState("");
+
+  async function hej() {
+    try {
+      await DbService.writeUserData("hej");
+      console.log("Done");
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
-    <div className="foodContainer">
-      <div>
-        <input type="text" className="add" onChange={(e) => {
-                setNewFood(e.target.value);
-              }}/>
-        <button>Add</button>
-      </div>
-      <div>
-        <p>What's for dinner?</p>
-        <button>Click here</button>
-      </div>
+      <div className="foodContainer">
+        <div>
+          <input
+            type="text"
+            className="add"
+            onChange={(e) => {
+              setNewFood(e.target.value);
+            }}
+          />
+          <button>Add</button>
+        </div>
+        <div>
+          <p>What's for dinner?</p>
+          <button
+            onClick={() => {
+              hej();
+            }}
+          >
+            Click here
+          </button>
+        </div>
       </div>
     </>
   );
